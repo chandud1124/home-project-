fd74d78ac240a511b95bf84a4b846bcbfd3f5c9e
chandu <chandu.d1124@gmail.com>
2025-07-12 07:58:06 +0530

    Delete Resume-Genie-main directory

--- Resume-Genie-main/Backend/config/mongoose.connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-function connectToDB() {
-    mongoose.connect(process.env.DB_CONNECT).then(() => {
-        console.log('Connected to MongoDB');
-    }).catch(err => console.log(err));
-}
-
-module.exports = connectToDB;
--- Resume-Genie-main/Backend/utils/bcryptPassword.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcrypt');
-
-module.exports.bcryptPassword = (password) => {
-    const salt = bcrypt.genSaltSync(10); // Generate salt
-    return bcrypt.hashSync(password, salt); // Hash the password and return
-};
